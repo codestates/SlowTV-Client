66014c998abd2c9edae162a9a99b8cf86dbcff09
jyshimmy <66928595+jyshimmy@users.noreply.github.com>
2021-01-14 10:30:38 +0900

    [Add] Login page component

---
+++ src/components/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="landing_page">
+                <Nav />
+                <button>Get Started!</button>
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
