8aa92a37e350534ce00ab45532ec6145520ff236
jyshimmy <66928595+jyshimmy@users.noreply.github.com>
2021-01-18 10:53:06 +0900

    [Add] login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,27 @@
-import React, { Component } from "react";
-import "../App.css";
-//test test test
+import React from "react";
+import "./Login.css";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="login">
-          <div className="login-half left">
-            <form>
-              <input type="text" placeholder="Enter email address" />
-              <input type="password" placeholder="Enter password" />
-              <button>Login</button>
-            </form>
-          </div>
-          <div className="bar bar-top"></div>
-          <span className="login-or">OR</span>
-          <div className="bar bar-bottom"></div>
-          <div className="login-half right">
-            <button>Login with GitHub</button>
-            <button>Login with Gmail</button>
-          </div>
+const Login = () => {
+  return (
+    <div>
+      <div className="login">
+        <div className="login-half left">
+          <form>
+            <input type="text" placeholder="Enter email address" />
+            <input type="password" placeholder="Enter password" />
+            <button>Login</button>
+          </form>
+        </div>
+        <div className="bar bar-top"></div>
+        <span className="login-or">OR</span>
+        <div className="bar bar-bottom"></div>
+        <div className="login-half right">
+          <button>Login with GitHub</button>
+          <button>Login with Gmail</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Login;
