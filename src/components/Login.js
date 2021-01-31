d49b1d496d835a6354085c80416d3164fbf48ff8
Minwoo Park <positive1attraction@gmail.com>
2021-01-31 19:37:41 +0900

    [Completed] All pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import LandingNavConatiner from "../containers/LandingNavContainers";
 import "./Login.css";
 import axios from "axios";
